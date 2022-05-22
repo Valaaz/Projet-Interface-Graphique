6b63db6d62b214a1ca17e2d551475bf484773388
Valaaz <val.zancoth@gmail.com>
2022-05-22 20:53:28 +0200

    add initialize method in parameters controller

--- src/mvc/controleur/ControleurParametres.java
+++ src/mvc/controleur/ControleurParametres.java
@@ -1,5 +1,31 @@
 package mvc.controleur;
 
-public class ControleurParametres {
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
+public class ControleurParametres {
+	@FXML
+	ChoiceBox<String> chBoxTheme;
+	
+	@FXML
+    private RadioButton radioBtnCinq, radioBtnDix, radioBtnFacile, radioBtnMoyen, radioBtnDifficile;
+	
+	@FXML
+	public void initialize() {
+		chBoxTheme.getItems().addAll("Tous les thèmes", "Animal", "Informatique", "Espace", "Nature", "Mobilier");
+		chBoxTheme.getSelectionModel().select(0);
+		
+		ToggleGroup groupeErreurMax = new ToggleGroup();
+		radioBtnCinq.setToggleGroup(groupeErreurMax);
+		radioBtnDix.setToggleGroup(groupeErreurMax);
+		radioBtnCinq.setSelected(true);
+		
+		ToggleGroup groupeDifficulte = new ToggleGroup();
+		radioBtnFacile.setToggleGroup(groupeDifficulte);
+		radioBtnMoyen.setToggleGroup(groupeDifficulte);
+		radioBtnDifficile.setToggleGroup(groupeDifficulte);
+		radioBtnMoyen.setSelected(true);
+	}
 }
